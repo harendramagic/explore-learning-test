e760f395b55ebbdb1b5fb71f87520f1983d55dc8
harendra <harendra.kumar@magicedtech.com>
2020-06-09 12:02:27 +0530

    Updated with default length for lastName field

--- src/main/java/com/magic/app/entity/Users.java
+++ src/main/java/com/magic/app/entity/Users.java
@@ -22,6 +22,6 @@ public class Users {
 	@Column(name="first_name", nullable = false, length = 250)
 	private String firstName;
 	
-	@Column(name="last_name", nullable = false, length = 5)
+	@Column(name="last_name", nullable = false, length = 250)
 	private String lastName;
 }
